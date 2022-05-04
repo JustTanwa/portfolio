ba5076c62cd4ae630c21cb59df1e967f5cca616a
Katakung95 <tanwasripan@hotmail.co.uk>
2022-05-04 16:53:42 +0100

    removed use on BR for better semantic HTML

--- src/projects-data.js
+++ src/projects-data.js
@@ -8,17 +8,20 @@ export const projects = [
 		livelink: 'https://furiousconsulting.netlify.app/',
 		github: 'https://github.com/JustTanwa/FuriousConsultant',
 		description: (
-			<p>
-				A modern style business consulting website coded from scratch using
-				HTML5, CSS3, JavaScript and jQuery. I am hosting this website on
-				Netlify, and you click on the link to see a live demo or view my code on
-				GitHub. <br />
-				<br />
-				The motivation for the website was to focus on building a landing page
-				with "Call to Action" buttons to draw the attention of the visitors. I
-				learnt about layout, creating dynamic content and using the Fetch API to
-				retreive external data.
-			</p>
+			<>
+				<p>
+					A modern style business consulting website coded from scratch using
+					HTML5, CSS3, JavaScript and jQuery. I am hosting this website on
+					Netlify, and you click on the link to see a live demo or view my code
+					on GitHub.{' '}
+				</p>
+				<p>
+					The motivation for the website was to focus on building a landing page
+					with "Call to Action" buttons to draw the attention of the visitors. I
+					learnt about layout, creating dynamic content and using the Fetch API
+					to retreive external data.
+				</p>
+			</>
 		),
 	},
 	{
@@ -27,34 +30,28 @@ export const projects = [
 		livelink: 'https://ticytacytoe.netlify.app/',
 		github: 'https://github.com/JustTanwa/tic-tac-toe',
 		description: (
-			<p>
-				I built this Tic-Tac-Toe game using ReactJS and styled the web app using
-				CSS3. Currently, you can play the game against another person or two
-				levels of difficulty against an AI. <br />
-				<br />
-				In the future, I plan to include some online functionality using
-				WebSocket.
-				<br />
-				<br />
-				You can checkout the game by clicking the Live Demo link, where the game
-				is being hosted on Netlify.
-			</p>
+			<>
+				<p>
+					I built this Tic-Tac-Toe game using ReactJS and styled the web app
+					using CSS3. Currently, you can play the game against another person or
+					two levels of difficulty against an AI.{' '}
+				</p>
+				<p>
+					In the future, I plan to include some online functionality using
+					WebSocket.
+				</p>
+				<p>
+					You can checkout the game by clicking the Live Demo link, where the
+					game is being hosted on Netlify.
+				</p>
+			</>
 		),
 	},
 	{
 		id: 3,
-		imgsrc: project2,
+		imgsrc: '',
 		livelink: 'https//www.google.com',
 		github: 'https://github.com/',
-		description: (
-			<p>
-				I used to travel a lot before the the current pandemic and I thought
-				when I start travelling again, I would want a way to create a website to
-				track all the trip information. <br />
-				<br />
-				So I built this travelling itinerary tracker using ReactJS, Bootstrap
-				and Firebase for authentication
-			</p>
-		),
+		description: <p>Working in progress.</p>,
 	},
 ];
